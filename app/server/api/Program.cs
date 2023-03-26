4c2d82b381d8d6dab79adf7b575aecad3e68b12f
tovDmitrij <DMXikka@gmail.com>
2023-03-26 15:35:04 +0300

    Инициализация API

---
+++ app/server/api/Program.cs
@@ -0,0 +1,42 @@
+namespace api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+            #region builder
+
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            #endregion
+
+
+
+            #region app
+
+            var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+
+            #endregion
+
+
+
+        }
+    }
+}
