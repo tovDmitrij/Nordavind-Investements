dd8ca590883099376cf9cec49111e9dd0f7f9d0a
Eightclockz <nikita.cozlow.0210@yandex.ru>
2023-04-09 21:05:09 +0300

    Круговая диаграмма

--- app/client/src/pages/directory/DirectoryPage.jsx
+++ app/client/src/pages/directory/DirectoryPage.jsx
@@ -3,17 +3,21 @@ import CurrenciesTable from "../../components/UI/tables/directoryTables/Currenci
 import TradeBotsTable from "../../components/UI/tables/directoryTables/TradeBotsTable/TradeBotsTable";
 import ConditionsTable from "../../components/UI/tables/directoryTables/ConditionsTable/ConditionsTable";
 import AccountTypesTable from "../../components/UI/tables/directoryTables/AccountTypesTable/AccountTypesTable";
+import PieChart from "../../components/UI/diagram/PieChart";
+
 
 const DirectoryPage = () =>{
     const[Currenciesdata, setCurrenciesdata] = useState([]);
     const[TradeBotsdata, setTradeBotsdata] = useState([]);
     const[AccountTypesdata, setAccountTypesdata] = useState([]);
     const[Conditionsdata, setConditionsdata] = useState([]);
+    
     const GenerateData = () =>{
         setCurrenciesdata([{Title:'fafas',Value:'vasgh'},{Title:'gkdld', Value:'fkdooe'}])
         setTradeBotsdata([{Title:'cjgk',Value:'fjkld'},{Title:'gkdfl', Value:'gfdfk'}])
         setAccountTypesdata([{Title:'iqofl',Value:'toytio'},{Title:'qwriot', Value:'cmxxz'}])
         setConditionsdata([{Title:'xznv', Description:'zjgl', Value:'eptpr'},{Title:'xmnv', Description:'topyp', Value:'zgjgkf'}])
+        
     }
     useEffect (() =>{GenerateData()}, [])
     return(
@@ -22,6 +26,7 @@ const DirectoryPage = () =>{
             <AccountTypesTable Data={AccountTypesdata}/>
             <TradeBotsTable Data={TradeBotsdata}/>
             <ConditionsTable Data={Conditionsdata}/>
+           <PieChart />
         </div>
         
 
