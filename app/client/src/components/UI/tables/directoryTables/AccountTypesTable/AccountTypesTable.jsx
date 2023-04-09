9a12af40348af27fb7238372fe125c29c4c6b0dc
Eightclockz <nikita.cozlow.0210@yandex.ru>
2023-04-09 13:08:15 +0300

    Сделал таблицы и страницу справочника

---
+++ app/client/src/components/UI/tables/directoryTables/AccountTypesTable/AccountTypesTable.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import MyButton from "../../../button/MyButton";
+
+const AccountTypesTable = ({Data, sample}) => {
+    return(
+       <div> 
+        <h1>Account types table</h1>
+         <table>
+            <thead>
+                <tr>
+                    <th>Title </th>
+                    <th>Description </th>
+                </tr>
+            </thead>
+            <tbody>
+                {Data.map((row)=> (
+                     <tr>
+                        <td>{row.Title} </td>
+                        <td>{row.Value} </td>
+                     </tr>
+                ))}
+            </tbody>
+         </table>
+         <MyButton children={'Добавить тип счёта'}/>
+       </div>
+    )
+}
+export default AccountTypesTable;
