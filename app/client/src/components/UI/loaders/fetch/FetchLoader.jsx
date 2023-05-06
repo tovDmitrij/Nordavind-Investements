b104b04ba63e54037a2d2e725d0bd03f3aa2b4cf
Dmitrij Kuzmin <86602542+tovDmitrij@users.noreply.github.com>
2023-05-06 20:01:14 +0300

    Delete app/client directory

--- app/client/src/components/UI/loaders/fetch/FetchLoader.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from './FetchLoader.module.css'
-
-
-const FetchLoader = () => { return(<div className={classes.loader}></div>) }
-
-
-export default FetchLoader
